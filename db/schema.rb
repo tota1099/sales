20040d62ae961d1e5f9e21c3c596b32c30e9f9b6
Renan Porto <renan.porto1099@gmail.com>
2022-07-01 21:06:27 -0300

    feat: add cancancan lib and configure it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_01_224347) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_01_233818) do
   create_table "addresses", force: :cascade do |t|
     t.string "country"
     t.string "city"
@@ -99,6 +99,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_01_224347) do
     t.text "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "addresses", "clients"
